ddeeacfc61066aa81fb0b2c8c0a89c0a3a8fbc2a
KVadim-K <kvadimk@yandex.ru>
2024-08-06 23:18:47 +0300

    Add Dz

--- ZeroCoder/news/models.py
+++ ZeroCoder/news/models.py
@@ -1,14 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class News_post(models.Model):
     title = models.CharField('Название новости', max_length=50)
     short_description = models.CharField('Краткое описание новости', max_length=200)
     text = models.TextField('Новость')
     pub_date = models.DateTimeField('Дата публикации')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')  # Внешний ключ на User
+    author_name = models.CharField(max_length=100, default='default_author')  # Дополнительное поле CharField
 
     def __str__(self):
         return self.title
 
     class Meta:
         verbose_name = 'Новость'
-        verbose_name_plural = ' Новости'
+        verbose_name_plural = 'Новости'
